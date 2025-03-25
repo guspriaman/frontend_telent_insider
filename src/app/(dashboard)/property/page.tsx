0d28524a7b87dde72ff7adbed067fa7d02f23c2a
guspriaman <email.guswarasi@gmail.com>
2025-03-25 11:57:20 +0700

    first commit

--- src/app/(dashboard)/property/page.tsx
+++ src/app/(dashboard)/property/page.tsx
@@ -7,9 +7,9 @@ import Property from '@/components/Property';
 const PropertyPage = () => {
   return (
     <div className='mt-20' >
-      <PropertyForm/>
-      <Property/>
       <PropertyLayout/>
+      <Property/>
+      <PropertyForm/>
     </div>
   );
 };
